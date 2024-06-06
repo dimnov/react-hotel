6d97031dcb811ff161013fd1b95bf0199de04169
dimnov <dimnov1512@gmail.com>
2024-06-06 23:56:48 +0300

    Created the app layout and its style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import styled from "styled-components";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import NewUsers from "./pages/Users";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
-    <div>
-      <H1>The Wild Oasis</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path={"dashboard"} element={<Dashboard />} />
+            <Route path={"bookings"} element={<Bookings />} />
+            <Route path={"cabins"} element={<Cabins />} />
+            <Route path={"users"} element={<NewUsers />} />
+            <Route path={"settings"} element={<Settings />} />
+            <Route path={"account"} element={<Account />} />
+          </Route>
+          <Route path={"login"} element={<Login />} />
+          <Route path={"*"} element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 

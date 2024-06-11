cc3a2229ac770a6cada07327cb45317594706784
dimnov <dimnov1512@gmail.com>
2024-06-11 20:56:36 +0300

    Add a way to upload an image

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
     const { data, error } = await supabase.from("cabins").select('*');
@@ -11,13 +11,28 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+    const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "");
+
+    const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
     const { data, error } = await supabase
         .from('cabins')
-        .insert([newCabin]);
+        .insert([{ ...newCabin, image: imagePath }]);
 
     if (error) {
         throw new Error(error);
     }
+
+    const { error: storageError } = await supabase
+        .storage
+        .from('cabin-images')
+        .upload(imageName, newCabin.image);
+
+    if (storageError) {
+        deleteCabin(data.id);
+        throw new Error('Cabin image could not be uploaded and the cabin was not created');
+    }
+
 
     return data;
 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://brxbjwhoanqvscdiiorh.supabase.co';
+
+export const supabaseUrl = 'https://brxbjwhoanqvscdiiorh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyeGJqd2hvYW5xdnNjZGlpb3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3MDg4ODQsImV4cCI6MjAzMzI4NDg4NH0.NSjnzJMPJAqmtkrxBLJrocl0Jgj5RmJh8zntEruDnJA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 

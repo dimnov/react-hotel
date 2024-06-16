f2282402cedaa60b198686802be8c62ad9591956
dimnov <dimnov1512@gmail.com>
2024-06-17 00:06:12 +0300

    implement pagination and get count from server

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,10 +1,11 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
-    .select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)");
+    .select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)", { count: 'exact' });
 
   if (filter) {
     query = query.eq(filter.field, filter.value);
@@ -16,14 +17,21 @@ export async function getBookings({ filter, sortBy }) {
     });
   }
 
-  const { data, error } = await query;
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Booking not found");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {

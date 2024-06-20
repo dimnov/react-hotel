c4987a0e840b10683b27aa7a92bc05e3f2caaf0d
dimnov <dimnov1512@gmail.com>
2024-06-20 16:22:31 +0300

    Create a function to change dark or light mode

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark-mode", isDarkMode);
+    document.documentElement.classList.toggle("light-mode", !isDarkMode);
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined) {
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+  }
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };

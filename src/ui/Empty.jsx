9b48f1b746a54e388a4ceccea0afda62041500e7
dimnov <dimnov1512@gmail.com>
2024-06-22 14:26:14 +0300

    Add style to Empty component

--- src/ui/Empty.jsx
+++ src/ui/Empty.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import Heading from "./Heading";
+
+const StyledEmpty = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 function Empty({ resource }) {
-  return <p>No {resource} could be found.</p>;
+  // return <p>No {resource} could be found.</p>;
+  return (
+    <StyledEmpty>
+      <Heading as="h2">No {resource} could be found.</Heading>
+    </StyledEmpty>
+  );
 }
 
 export default Empty;
